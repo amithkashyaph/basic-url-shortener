8089e3db1bfed2cf97b47a0d652c10994e116a9e
amithkashyap <amithkashyaprv@gmail.com>
2025-10-21 00:09:37 +0530

    added fields in user entity

--- src/main/java/com/project/url_shortener/entities/User.java
+++ src/main/java/com/project/url_shortener/entities/User.java
@@ -1,4 +1,14 @@
 package com.project.url_shortener.entities;
 
+import com.project.url_shortener.enums.Role;
+
+import java.time.LocalDateTime;
+
 public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private Role role;
+    private LocalDateTime createdAt;
 }
