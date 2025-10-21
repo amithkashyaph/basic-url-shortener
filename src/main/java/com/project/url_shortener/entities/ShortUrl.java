b8921ed3571d0a19d8195fc2a1396323b66250a9
amithkashyap <amithkashyaprv@gmail.com>
2025-10-22 00:10:16 +0530

    added fields in short url entity

--- src/main/java/com/project/url_shortener/entities/ShortUrl.java
+++ src/main/java/com/project/url_shortener/entities/ShortUrl.java
@@ -1,4 +1,15 @@
 package com.project.url_shortener.entities;
 
+import java.time.LocalDateTime;
+
 public class ShortUrl {
+    private Long id;
+    private String shortKey;
+    private String originalUrl;
+    private User user;
+    private Boolean isPrivate;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private Long clickCount;
+
 }
