d28ff330685344545c9823b72937aad71957edeb
amithkashyap <amithkashyaprv@gmail.com>
2025-10-21 23:36:02 +0530

    annotated fields in user entity

--- src/main/java/com/project/url_shortener/entities/User.java
+++ src/main/java/com/project/url_shortener/entities/User.java
@@ -1,14 +1,21 @@
 package com.project.url_shortener.entities;
 
 import com.project.url_shortener.enums.Role;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
     private String name;
     private Role role;
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
