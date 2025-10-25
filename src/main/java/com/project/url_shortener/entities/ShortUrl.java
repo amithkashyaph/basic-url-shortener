e0ae4896410d04b64ddda8de032da648dd0428ec
amithkashyap <amithkashyaprv@gmail.com>
2025-10-25 21:09:45 +0530

    annotated short url entity

--- src/main/java/com/project/url_shortener/entities/ShortUrl.java
+++ src/main/java/com/project/url_shortener/entities/ShortUrl.java
@@ -1,14 +1,29 @@
 package com.project.url_shortener.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "short_urls")
+@Getter
+@Setter
 public class ShortUrl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String shortKey;
+    @Column(nullable = false)
     private String originalUrl;
-    private User user;
+    @ManyToOne
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+    @Column(nullable = false)
     private Boolean isPrivate;
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
     private LocalDateTime expiresAt;
     private Long clickCount;
 
