0527193c134dae76a529f9a5146380cd179fcf61
amithkashyap <amithkashyaprv@gmail.com>
2025-10-27 01:30:14 +0530

    added script to update admin user's password

---
+++ src/main/resources/db/migration/V5__update_admin_passwords.sql
@@ -0,0 +1,2 @@
+UPDATE users SET password = '$2a$10$MNuz4gw8SI6cmkb7gAzb5uhMbgv4oz.QhYHRZsM3sD7h8LKP405ku' WHERE id = 3;
+UPDATE users SET password = '$2a$10$MNuz4gw8SI6cmkb7gAzb5uhMbgv4oz.QhYHRZsM3sD7h8LKP405ku' WHERE id = 4;
