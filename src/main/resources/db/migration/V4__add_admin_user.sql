ece701c713076f051e23e436253a0a7753f53c0c
amithkashyap <amithkashyaprv@gmail.com>
2025-10-27 01:29:57 +0530

    added script to add admin users

---
+++ src/main/resources/db/migration/V4__add_admin_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, password, name, role)
+    VALUES ('admin@admin.com', 'admin123', 'Admin', 'ROLE_ADMIN'),
+           ('admin2@admin.com', 'admin123', 'Admin 2', 'ROLE_ADMIN');
