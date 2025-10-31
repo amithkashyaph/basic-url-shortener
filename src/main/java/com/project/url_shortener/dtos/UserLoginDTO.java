9d35e38ee272b8e26ba9006812bbf340b02b0560
amithkashyap <amithkashyaprv@gmail.com>
2025-10-31 23:14:49 +0530

    added fields in user login DTO

--- src/main/java/com/project/url_shortener/dtos/UserLoginDTO.java
+++ src/main/java/com/project/url_shortener/dtos/UserLoginDTO.java
@@ -2,5 +2,7 @@ package com.project.url_shortener.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record UserLoginDTO() {
+public record UserLoginDTO(
+        String email,
+        String password) {
 }
