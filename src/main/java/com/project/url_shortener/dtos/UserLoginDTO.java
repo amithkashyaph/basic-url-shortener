35d6a941bad491c5f853f1a6e35f4236d00266cf
amithkashyap <amithkashyaprv@gmail.com>
2025-10-31 23:15:05 +0530

    annotated fields in user login DTO

--- src/main/java/com/project/url_shortener/dtos/UserLoginDTO.java
+++ src/main/java/com/project/url_shortener/dtos/UserLoginDTO.java
@@ -3,6 +3,8 @@ package com.project.url_shortener.dtos;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserLoginDTO(
+        @NotBlank(message = "Email address cannot be empty")
         String email,
+        @NotBlank(message = "Password cannot be empty")
         String password) {
 }
