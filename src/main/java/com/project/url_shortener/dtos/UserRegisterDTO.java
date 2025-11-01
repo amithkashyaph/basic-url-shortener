14039f65f99dbef5258b22786f8ea846a08d2476
amithkashyap <amithkashyaprv@gmail.com>
2025-11-01 05:57:23 +0530

    added initial User register DTO

---
+++ src/main/java/com/project/url_shortener/dtos/UserRegisterDTO.java
@@ -0,0 +1,11 @@
+package com.project.url_shortener.dtos;
+
+import com.project.url_shortener.enums.Role;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegisterDTO {
+}
