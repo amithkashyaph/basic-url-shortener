c5c7a645696abc21b5cc18521e9f9199cde83dd5
amithkashyap <amithkashyaprv@gmail.com>
2025-11-02 23:49:07 +0530

    implemented user already exist exception

--- src/main/java/com/project/url_shortener/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/project/url_shortener/exceptions/UserAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package com.project.url_shortener.exceptions;
 
 public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
 }
