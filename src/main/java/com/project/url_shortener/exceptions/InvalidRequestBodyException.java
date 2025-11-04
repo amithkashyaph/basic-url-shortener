d9662c9de7712c63b36221a244cc44505780627b
amithkashyap <amithkashyaprv@gmail.com>
2025-11-04 13:59:56 +0530

    implemented invalid request body exception

--- src/main/java/com/project/url_shortener/exceptions/InvalidRequestBodyException.java
+++ src/main/java/com/project/url_shortener/exceptions/InvalidRequestBodyException.java
@@ -2,4 +2,7 @@ package com.project.url_shortener.exceptions;
 
 public class InvalidRequestBodyException extends Exception {
 
+    public InvalidRequestBodyException(String message) {
+        super(message);
+    }
 }
