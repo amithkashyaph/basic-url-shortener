bdb804736aa9f3f56aa399ac430ed74102cffc24
amithkashyap <amithkashyaprv@gmail.com>
2025-11-07 23:16:06 +0530

    added toShortUrlDTO entity mapper method

--- src/main/java/com/project/url_shortener/utils/EntityMapper.java
+++ src/main/java/com/project/url_shortener/utils/EntityMapper.java
@@ -9,6 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class EntityMapper {
 
+    public ShortUrlDTO toShortUrlDTO(ShortUrl shortUrl) {
+        UserDTO userDTO = null;
+        if (shortUrl.getCreatedBy() != null) {
+            userDTO = toUserDTO(shortUrl.getCreatedBy());
+        }
+
+        return new ShortUrlDTO(shortUrl.getId(), shortUrl.getShortKey(), shortUrl.getOriginalUrl(), shortUrl.getExpiresAt(), userDTO, shortUrl.getIsPrivate(), shortUrl.getCreatedAt(), shortUrl.getClickCount());
+    }
+
     public UserDTO toUserDTO(User createdBy) {
         return new UserDTO(createdBy.getId(), createdBy.getName());
     }
