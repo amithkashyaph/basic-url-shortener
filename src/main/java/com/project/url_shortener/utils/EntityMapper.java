7a8017799029d1074ee698be4a8cfa5995c91cda
amithkashyap <amithkashyaprv@gmail.com>
2025-11-07 23:14:31 +0530

    added initial entity mapper component

---
+++ src/main/java/com/project/url_shortener/utils/EntityMapper.java
@@ -0,0 +1,12 @@
+package com.project.url_shortener.utils;
+
+import com.project.url_shortener.dtos.ShortUrlDTO;
+import com.project.url_shortener.dtos.UserDTO;
+import com.project.url_shortener.entities.ShortUrl;
+import com.project.url_shortener.entities.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EntityMapper {
+
+}
