11e92d8cb31932c663015acae9fb96b805313b0e
amithkashyap <amithkashyaprv@gmail.com>
2025-11-08 22:37:09 +0530

    added initial short url service interface

---
+++ src/main/java/com/project/url_shortener/services/interfaces/ShortUrlService.java
@@ -0,0 +1,12 @@
+package com.project.url_shortener.services.interfaces;
+
+import com.project.url_shortener.dtos.CreateShortUrlDTO;
+import com.project.url_shortener.dtos.PagedResult;
+import com.project.url_shortener.dtos.ShortUrlDTO;
+import com.project.url_shortener.entities.ShortUrl;
+import com.project.url_shortener.exceptions.ShortKeyDoesNotExistException;
+
+import java.util.List;
+
+public interface ShortUrlService {
+}
