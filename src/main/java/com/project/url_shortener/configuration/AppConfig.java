6ecd7cdc2cf0501fb09e6e6b50d135c45e3b77d0
amithkashyap <amithkashyaprv@gmail.com>
2025-12-16 00:12:04 +0530

    added initial app config class

---
+++ src/main/java/com/project/url_shortener/configuration/AppConfig.java
@@ -0,0 +1,26 @@
+package com.project.url_shortener.configuration;
+
+import com.project.url_shortener.security.SecurityUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+    private SecurityUserDetailsService securityUserDetailsService;
+    private PasswordEncoder passwordEncoder;
+
+    public AppConfig(SecurityUserDetailsService securityUserDetailsService, PasswordEncoder passwordEncoder) {
+        this.securityUserDetailsService = securityUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+}
+
