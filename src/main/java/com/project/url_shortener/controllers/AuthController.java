62a19a68423bd76440553c23d187757fd98f2e5b
amithkashyap <amithkashyaprv@gmail.com>
2025-12-16 22:33:47 +0530

    added initial auth controller

---
+++ src/main/java/com/project/url_shortener/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.project.url_shortener.controllers;
+
+import com.project.url_shortener.dtos.UserDTO;
+import com.project.url_shortener.dtos.UserLoginDTO;
+import com.project.url_shortener.dtos.UserRegisterDTO;
+import com.project.url_shortener.exceptions.UserAlreadyExistsException;
+import com.project.url_shortener.services.interfaces.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private UserService userService;
+
+    public AuthController(AuthenticationManager authenticationManager, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+    }
+}
+
