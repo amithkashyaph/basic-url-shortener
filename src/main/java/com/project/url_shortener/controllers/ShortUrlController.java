e3bcd4abfdbb294ccbee16b3afe6bacb761a9660
amithkashyap <amithkashyaprv@gmail.com>
2025-12-19 22:07:24 +0530

    initialized short url controller component

--- src/main/java/com/project/url_shortener/controllers/ShortUrlController.java
+++ src/main/java/com/project/url_shortener/controllers/ShortUrlController.java
@@ -1,5 +1,29 @@
 package com.project.url_shortener.controllers;
 
+import com.project.url_shortener.configuration.ApplicationProperties;
+import com.project.url_shortener.dtos.CreateShortUrlDTO;
+import com.project.url_shortener.dtos.PagedResult;
+import com.project.url_shortener.dtos.ShortUrlDTO;
+import com.project.url_shortener.entities.ShortUrl;
+import com.project.url_shortener.exceptions.ShortKeyDoesNotExistException;
+import com.project.url_shortener.services.interfaces.ShortUrlService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ShortUrlController {
+
+    private ShortUrlService shortUrlService;
+    private ApplicationProperties applicationProperties;
+
+    @Autowired
+    ShortUrlController(ShortUrlService shortUrlService, ApplicationProperties applicationProperties) {
+        this.shortUrlService = shortUrlService;
+        this.applicationProperties = applicationProperties;
+    }
+
 
 }
