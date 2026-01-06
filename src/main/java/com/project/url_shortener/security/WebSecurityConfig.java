d9e7b06ec3d06c766ac4e912ad7acb3f6a9e8927
amithkashyap <amithkashyaprv@gmail.com>
2026-01-06 23:41:41 +0530

    added web security config class

---
+++ src/main/java/com/project/url_shortener/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.project.url_shortener.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    private SecurityUserDetailsService securityUserDetailsService;
+
+    public WebSecurityConfig(SecurityUserDetailsService securityUserDetailsService) {
+        this.securityUserDetailsService = securityUserDetailsService;
+    }
+
+
+}
