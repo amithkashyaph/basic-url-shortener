a43c556fe6685815b74174ef54d661f4c5e32b53
amithkashyap <amithkashyaprv@gmail.com>
2026-01-12 23:47:43 +0530

    added initial user service impl class

---
+++ src/main/java/com/project/url_shortener/services/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.project.url_shortener.services;
+
+import com.project.url_shortener.dtos.*;
+import com.project.url_shortener.entities.User;
+import com.project.url_shortener.enums.Role;
+import com.project.url_shortener.exceptions.InvalidCredentialException;
+import com.project.url_shortener.exceptions.InvalidRequestBodyException;
+import com.project.url_shortener.exceptions.UserAlreadyExistsException;
+import com.project.url_shortener.repositories.ShortUrlRespository;
+import com.project.url_shortener.repositories.UserRepository;
+import com.project.url_shortener.repositories.UserRepositoryJdbcClient;
+import com.project.url_shortener.services.interfaces.UserService;
+import com.project.url_shortener.utils.EntityMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+}
